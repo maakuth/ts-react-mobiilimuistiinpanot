2fda98274f9318038c93611c9e6314e3eb899280
Markus Vuorio <markus.vuorio@iki.fi>
2025-10-17 21:20:16 +0300

    feat: Prepare for Github actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace `<YOUR_REPOSITORY_NAME>` with the name of your GitHub repository.
-  // For example, if your repository URL is `https://github.com/user/my-voice-app`,
-  // the base should be `/my-voice-app/`.
-  base: '/<YOUR_REPOSITORY_NAME>/',
+  base: '//ts-react-mobiilimuistiinpanot/',
 })
